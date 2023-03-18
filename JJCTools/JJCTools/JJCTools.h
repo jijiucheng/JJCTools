7573ea4949fb91338656df4af18ce7916c91b275
jcji <jcji@iflytek.com>
2023-03-18 12:37:50 +0800

    build：初始化工程

---
+++ JJCTools/JJCTools/JJCTools.h
@@ -0,0 +1,18 @@
+//
+//  JJCTools.h
+//  JJCTools
+//
+//  Created by mxgx on 2023/3/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JJCTools.
+FOUNDATION_EXPORT double JJCToolsVersionNumber;
+
+//! Project version string for JJCTools.
+FOUNDATION_EXPORT const unsigned char JJCToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJCTools/PublicHeader.h>
+
+
