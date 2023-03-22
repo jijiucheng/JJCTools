7b0ba974267c250d47216e66c816b5aad399acc1
jcji <302926124@qq.com>
2023-03-22 14:58:25 +0800

    feat：1、优化 Bundle 扩展代码

--- JJCTools/JJCTools/Sources/Extension/Bundle+JJCAPI.swift
+++ JJCTools/JJCTools/Sources/Extension/Bundle+JJCAPI.swift
@@ -10,18 +10,74 @@ import UIKit
 
 //MARK: - Bundle - 获取资源文件
 extension Bundle {
-    /// Bundle - 根据 bundle 名称获取对应 Bundle
-    public static func jjc_bundle(_ bundle: String) -> Bundle? {
-        return Bundle(path: Bundle.main.path(forResource: bundle, ofType: "bundle") ?? "")
+    /// Bundle - 获取主工程或子模块的主 Bundle
+    ///
+    /// 静态库 - 编译后获取的都是主工程的 Bundle
+    /// 动态库 - 通过 bundleForClass 可以获取动态库对应的 Bundle，即子模块的 Bundle
+    /// 参考链接：https://www.jianshu.com/p/b64ff9d8e7ce
+    public static func jjc_mainBundle(_ isModule: Bool = false, objcClass: AnyClass? = nil) -> Bundle? {
+        if isModule, let tempObjcClass = objcClass {
+            return Bundle(for: tempObjcClass)
+        } else {
+            return Bundle.main
+        }
     }
     
-    /// Bundle - 根据 bundle 名称获取对应内部资源文件路径
-    public static func jjc_bundleFile(resource: String, ofType type: String, bundle: String) -> String? {
-        return Bundle.jjc_bundle(bundle)?.path(forResource: resource, ofType: type)
+    /// Bundle - 根据 Bundle 名称获取对应的 Bundle 文件
+    public static func jjc_bundle(bundle: String? = nil, isModule: Bool = false, objcClass: AnyClass? = nil) -> Bundle? {
+        if let mainBundle = Bundle.jjc_mainBundle(isModule, objcClass: objcClass),
+            let path = mainBundle.path(forResource: bundle, ofType: "bundle") {
+            return Bundle(path: path)
+        }
+        return nil
     }
     
-    /// Bundle - 根据 bundle 名称获取对应内部图片文件路径
-    public static func jjc_bundleImage(resource: String, ofType type: String, bundle: String) -> UIImage? {
-        return UIImage(contentsOfFile: Bundle.jjc_bundle(bundle)?.path(forResource: resource, ofType: type) ?? "")
+    /// Bundle - 根据 Bundle 名称获取内部资源文件路径
+    public static func jjc_bundle(file: String, ofType type: String, bundle: String? = nil, isModule: Bool = false, objcClass: AnyClass? = nil) -> String? {
+        if let fileBundle = Bundle.jjc_bundle(bundle: bundle, isModule: isModule, objcClass: objcClass) {
+            return fileBundle.path(forResource: file, ofType: type)
+        }
+        return nil
+    }
+    
+    /// Bundle - 根据 Bundle 名称获取内部图片文件
+    public static func jjc_bundle(image imageName: String, ofType type: String, bundle: String? = nil, isModule: Bool = false, objcClass: AnyClass? = nil) -> UIImage? {
+        if let path = Bundle.jjc_bundle(file: imageName, ofType: type, bundle: bundle, isModule: isModule, objcClass: objcClass) {
+            return UIImage(contentsOfFile: path)
+        }
+        return nil
+    }
+}
+
+//MARK: - Bundle - JJCTools 内部使用
+extension Bundle {
+    /// Bundle - JJCTools - 获取主 Bundle
+    static func jjc_mainBundleByJJCTools() -> Bundle? {
+        return Bundle(for: JJCGlobalClass.self)
+    }
+    
+    /// Bundle - JJCTools - 根据 Bundle 名称获取对应的 Bundle 文件
+    static func jjc_bundleByJJCTools(bundle: String? = nil) -> Bundle? {
+        if let mainBundle = Bundle.jjc_mainBundleByJJCTools(),
+            let path = mainBundle.path(forResource: bundle, ofType: "bundle") {
+            return Bundle(path: path)
+        }
+        return nil
+    }
+    
+    /// Bundle - JJCTools - 根据 Bundle 名称获取内部资源文件路径
+    static func jjc_bundleByJJCTools(file: String, ofType type: String, bundle: String? = nil) -> String? {
+        if let fileBundle = Bundle.jjc_bundleByJJCTools(bundle: bundle) {
+            return fileBundle.path(forResource: file, ofType: type)
+        }
+        return nil
+    }
+    
+    /// Bundle - JJCTools - 根据 Bundle 名称获取内部图片文件
+    static func jjc_bundleByJJCTools(image imageName: String, ofType type: String, bundle: String? = nil) -> UIImage? {
+        if let path = Bundle.jjc_bundleByJJCTools(file: imageName, ofType: type, bundle: bundle) {
+            return UIImage(contentsOfFile: path)
+        }
+        return nil
     }
 }
