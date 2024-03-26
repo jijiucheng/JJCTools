b1e1abf961900607e0199679e3f65f25cfb21ef8
jcji <302926124@qq.com>
2024-03-27 00:09:44 +0800

    perf：1、优化 TabBar 显示问题

--- JJCTools/JJCTools/Sources/Controller/JJCTabBarController.swift
+++ JJCTools/JJCTools/Sources/Controller/JJCTabBarController.swift
@@ -13,11 +13,36 @@ open class JJCTabBarController: UITabBarController {
         super.viewDidLoad()
         
         selectedIndex = 0
+        jjc_setTopLineColor(JJC_RGBA_255(230, 230, 230))
         jjc_addAllChildController()
     }
 }
 
 extension JJCTabBarController {
+    /// 设置 TabBar 顶部分割线颜色
+    /// 参考链接：https://www.jianshu.com/p/5a63ad6229fd
+    @objc open func jjc_setTopLineColor(_ color: UIColor?) {
+        if let tempColor = color {
+            let view = UIView(frame: CGRect(x: 0, y: -0.5, width: tabBar.bounds.width, height: 0.5))
+            view.backgroundColor = tempColor
+            UITabBar.appearance().insertSubview(view, at: 0)
+            
+//            UITabBar.appearance().shadowImage = UIImage()
+//            UITabBar.appearance().backgroundImage = UIImage()
+//            UITabBar.appearance().backgroundColor = UIColor.white
+//            
+//            let redLine = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.bounds.width, height: 1))
+//            redLine.backgroundColor = tempColor
+//
+//            UIGraphicsBeginImageContextWithOptions(tabBar.bounds.size, false, 0)
+//            redLine.layer.render(in: UIGraphicsGetCurrentContext()!)
+//            let image = UIGraphicsGetImageFromCurrentImageContext()
+//            UIGraphicsEndImageContext()
+//            
+//            UITabBar.appearance().backgroundImage = image
+        }
+    }
+    
     /// 添加所有子视图
     @objc open func jjc_addAllChildController() {}
     
