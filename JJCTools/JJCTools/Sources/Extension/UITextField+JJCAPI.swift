9730249b03bb0ca068cabd25c78a84daf04e777c
jcji <302926124@qq.com>
2024-03-31 23:12:31 +0800

    perf：1、优化 UITextField API

--- JJCTools/JJCTools/Sources/Extension/UITextField+JJCAPI.swift
+++ JJCTools/JJCTools/Sources/Extension/UITextField+JJCAPI.swift
@@ -9,9 +9,16 @@ import Foundation
 import UIKit
 
 extension UITextField {
-    /// UITextField - 添加文本左侧间距
-    public func jjc_leftMargin(_ margin: CGFloat) {
-        leftView = UIView(frame: CGRect(x: 0, y: 0, width: margin, height: 0.1))
-        leftViewMode = .always
+    /// UITextField - 添加文本两侧间距
+    public func jjc_margin(_ left: CGFloat? = nil, _ right: CGFloat? = nil) {
+        guard left != nil && right != nil else { return }
+        if let leftMargin = left {
+            leftView = UIView(frame: CGRect(x: 0, y: 0, width: leftMargin, height: 0.1))
+            leftViewMode = .always
+        }
+        if let rightMargin = right {
+            rightView = UIView(frame: CGRect(x: 0, y: 0, width: rightMargin, height: 0.1))
+            rightViewMode = .always
+        }
     }
 }
